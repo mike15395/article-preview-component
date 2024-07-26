961f3b8ffeb0fd85b2fa8ddeb5aa0d847059f95a
mike15395 <mikhildesai@gmail.com>
2024-07-26 17:02:12 +0530

    dektop done mobile partial

---
+++ index.js
@@ -0,0 +1,8 @@
+// const shareButton = document.getElementById("share-button");
+// let shareTooltip = document.getElementById("tooltip");
+// shareButton.addEventListener("click", function () {
+//   console.log(shareTooltip.style.display);
+//   shareTooltip.style.display === "none" || shareTooltip.style.display === ""
+//     ? (shareTooltip.style.display = "flex")
+//     : (shareTooltip.style.display = "none");
+// });
