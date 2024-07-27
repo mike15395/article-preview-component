308fc96cafed3ca8c6a135ce4e16406bbecdfc18
mike15395 <mikhildesai@gmail.com>
2024-07-27 15:57:57 +0530

    mobile-code-changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // const shareButton = document.getElementById("share-button");
+// console.log(shareButton.style.display);
 // let shareTooltip = document.getElementById("tooltip");
 // shareButton.addEventListener("click", function () {
 //   console.log(shareTooltip.style.display);
@@ -6,3 +7,20 @@
 //     ? (shareTooltip.style.display = "flex")
 //     : (shareTooltip.style.display = "none");
 // });
+
+// let isMobile = window.screen.width <= 820;
+// console.log(window.screen.width, "width");
+// console.log(window.screen.height, "height");
+// console.log(isMobile, "is mobile");
+// if (isMobile) {
+//   // Mobile-specific JavaScript code
+//   let mobileFooter = document.getElementById("mobile-footer");
+//   let shareButton = document.getElementById("share-button");
+//   let lastSection = document.getElementById("last-text-section");
+//   let mobileShareButton = document.getElementById("mobile-share-button");
+
+//   // mobileFooter.style.display = "none";
+//   shareButton.addEventListener("click", function () {
+//     lastSection.className = "mobile-footer";
+//   });
+// }
